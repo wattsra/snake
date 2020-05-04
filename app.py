7b75ef4f72e7c79d32d67019e082181b2ab1a02d
Robert Watts <poloking@gmail.com>
2020-05-04 17:08:53 +0100

    starting new python snake project

---
+++ app.py
@@ -0,0 +1,30 @@
+dificulty = 4
+max_x = 100
+maxy_y = 100
+
+
+class gameManager():
+    def __init__(self):
+
+
+    def tick(self):
+
+
+    def update_score(self):
+
+
+
+class snake():
+    def __init__(self):
+        x = max_x/2#xposition
+        y = max_y/2#y position
+        v = 0.1 * (dificulty **2) #velocity multiplied by the square of the difficult number
+        length = 2
+
+    def move(self):
+
+
+    def collision(self):
+
+
+
