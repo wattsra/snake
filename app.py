1f9cd392e37cc5ae482c18f37389ba77ac7fc9c3
Robert Watts <poloking@gmail.com>
2020-05-11 14:45:58 +0100

    added startmenu, highscores and increasing speed. buggy, but complete

--- app.py
+++ app.py
@@ -1,84 +1,40 @@
-from time import perf_counter
-from typing import List, Any
 import pygame
+import random
 
-
-dificulty = 4
-max_x = 100
-max_y = 100
-scale_factor = 4
-snake_size = 5
+snake_size = scale_factor = 4
+scale_x = 20
+scale_y = 20
+max_x = scale_x * scale_factor
+max_y = scale_y * scale_factor
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 RED = (255,0,0)
 loc = []
-#fruits = []
-
-# class GameManager():
-#     def __init__(self):
-#         self.timer = None
-#         global loc
-#         pass
-#
-#     def start_timer(self):
-#         self.timer = perf_counter()
-#         #print(self.timer)
-#         return self.timer
-#
-#     def current_tick(self):
-#         tick = perf_counter() - self.timer
-#         return tick
-#
-#     def stop_timer(self):
-#         if self.timer is None:
-#             raise TimerError("Timer not running")
-#         self.timer = None
-#
-#     def tick(self):
-#         global loc
-#         while self.timer:
-#             if self.current_tick() >= 0.1:
-#                 #print("tick")
-#                 self.stop_timer()
-#                 self.start_timer()
-#                 self.update_score()
-#                 self.snake.controls()
-#                 self.snake.move()  # need to create snake class with move function
-#
-#     def update_score(self):
-#         print("snakehead",self.snake.head())
-#         if self.snake.head in self.fruits:
-#             self.score +=1
-#         pass
-#
-#     def start(self):
-#         self.start_timer()
-#         self.snake = Snake()
-#         self.fruits = Fruits().fruits
-
+startingspeed = 10
 class Snake():
     def __init__(self):
         global loc
-        start_x = max_x//2#xposition
-        start_y = max_y//2#y position
-        v = 0.1 * (dificulty ** 2) #velocity multiplied by the square of the difficult number
+        start_x = int(max_x//2)#xposition
+        start_y = int(max_y//2)#y position
         self.dir_x=0
         self.dir_y=1
-        #start_length = 2 # for now the snake is only one cube
         loc = [(start_x,start_y),]
-        print(loc)
 
 #### NEED TO SWAP X AND Y DIRECTIONS
 
     def move(self):
-        global loc
+        global loc, f, s, run,game
         n = len(loc)
         new_locs = [0]*n
         for i in range(n-1):
             new_locs[i+1] = loc[i]
         for event in pygame.event.get():
-            #print(event)
-            #if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
+            if event.type == pygame.KEYDOWN: # or event.type == pygame.KEYUP
+                if event.key == pygame.K_q:
+                    game = False
+            # else:
+            #     if event.key == pygame.K_q:
+            #         run = True
             keys = pygame.key.get_pressed()
             if keys[pygame.K_DOWN]:
                 if self.dir_y != -1:
@@ -98,62 +54,166 @@ class Snake():
                     self.dir_x = 1
         #for i in range(len(loc)):
         x,y = loc[0][0], loc[0][1]
-        if (x <= 0 and self.dir_x ==-1):
+        if (x < 0):
+            #and self.dir_x ==-1: #
             new_locs[0] = ((max_x-snake_size), (y))
-        elif (y <= 0 and self.dir_y ==-1):
-            new_locs[0] = (x, (max_y-1))
-        elif (x >= (max_x-snake_size) and self.dir_x ==1):
+        elif (y < 0):
+            #and self.dir_y ==-1: #
+            new_locs[0] = (x, (max_y-snake_size))
+        elif (x >= (max_x-snake_size))and self.dir_x ==1: #
             new_locs[0] = ((0), (y))
-        elif (y >= (max_y-snake_size) and self.dir_y ==1):
+        elif (y > (max_y-snake_size)):
+            #and self.dir_y ==1: #
             new_locs[0] = (x, (0))
         else:
             new_locs[0] = ((loc[0][0] + self.dir_x*snake_size),(loc[0][1] + self.dir_y*snake_size))
+        ### check for eating fruits ###
+        for fruit in f.fruits:
+            if (fruit[0]-snake_size < loc[0][0] < (fruit[0]+snake_size)) and fruit[1]-snake_size < loc[0][1] < (fruit[1]+snake_size):
+                new_locs.append(loc[-1])
+                f.eat(fruit)
+        ### check for crashing###
+        for i in range(len(new_locs)-1):
+            if new_locs[i+1][0] == new_locs[0][0] and new_locs[i+1][1] == new_locs[0][1]:
+                game = False
         loc = new_locs
 
     def draw(self,screen):
-        print(loc)
         for i in range(len(loc)):
             pygame.draw.rect(screen, WHITE, (loc[i][0], loc[i][1], snake_size, snake_size))
             if i == 0:
-                pygame.draw.rect(screen, RED,
-                                 (loc[i][0]+(snake_size/4), loc[i][1]+(snake_size/4), snake_size/4, snake_size/4))
-
+                pygame.draw.rect(screen, RED,(loc[i][0]+(snake_size/4), loc[i][1]+(snake_size/4), snake_size/4, snake_size/4))
     def eat(self):
         loc.append((loc[-1][0],loc[-1][1]))
 
+def drawText(screen,text,size,font,color,bgcolor=None,center=False,padding=None):
+    if padding:
+        print("padding for ",text)
+        padx=padding[0]
+        pady = padding[1]
+    else:
+        padx,pady = 0,int(size)
+    font = pygame.font.Font(font, size)
+    text = font.render(text,True,color,bgcolor)
+    textRect = text.get_rect()
+    if center:
+        if center in ["x","xy","yx"]:
+            textRect.centerx = screen.get_rect().centerx + padx
+            textRect.centery = pady
+        if center in ["y","xy","yx"]:
+            textRect.centery = screen.get_rect().centerx + pady
+            textRect.centerx = padx
+
+    screen.blit(text,textRect)
+
+
+
 def drawWindow(screen):
-    global s, win
+    global s, win,score,level
     screen.fill(BLACK)
     s.draw(screen)
     f.draw(screen)
     win.blit(pygame.transform.scale(screen, win.get_rect().size), (0, 0))
+    drawText(win,"Score: "+ str(score),28,'Roboto-Bold.ttf',WHITE,center="x")
+    drawText(win,"level: "+ str(level),12,'Roboto-Bold.ttf',WHITE,bgcolor=RED)
+
     pygame.display.update()
 
+def drawLoseWindow(screen):
+    global s, win,score,high
+    font = pygame.font.Font('Roboto-Bold.ttf', 32)
+    text = font.render('YOU LOSE', True, WHITE, RED)
+    textRect = text.get_rect()
+    textRect.center = win.get_rect().center
+    win.blit(text, textRect)
+    font2 = pygame.font.Font('Roboto-Bold.ttf', 20)
+    text2 = font2.render('Score: '+ str(score), True, WHITE,BLACK)
+    text2Rect = text2.get_rect()
+    text2Rect.center = win.get_rect().center
+    text2Rect.centery = text2Rect.centery + 50
+    win.blit(text2, text2Rect)
+    text3 = font2.render('High Score: '+ str(high), True, WHITE,BLACK)
+    text3Rect = text3.get_rect()
+    text3Rect.center = win.get_rect().center
+    text3Rect.centery = text3Rect.centery + 70
+    win.blit(text3, text3Rect)
+    pygame.display.update()
 
 class Fruits():
     def __init__(self):
-        self.fruits = [(3,6)]
+        self.fruits = []
+
+        #self.fruits = [(6*scale_factor,6*scale_factor)]
     def add_random(self):
-        self.fruits.append((4,5))
-        pass
+        global loc
+        check = True
+        while check:
+            new_fruit = (random.randint(1,scale_x-1)*scale_factor,random.randint(1,scale_y-1)*scale_factor)
+            if new_fruit not in loc[1:]:
+                self.fruits.append(new_fruit)
+                check = False
     def draw(self,screen):
         for i in range(len(self.fruits)):
             pygame.draw.rect(screen, RED, (self.fruits[i][0], self.fruits[i][1], snake_size, snake_size))
+    def eat(self,fruit):
+        global score
+        self.fruits.pop(self.fruits.index(fruit))
+        score += 100
+        self.add_random()
+
+def speedupdate():
+    global score
+    level = (score // 400) +1
+    speed = int(level + startingspeed)
+    return speed, level
+
+def startmenu(screen):
+    global win, game, score,run
+    screen.fill(BLACK)
+    win.blit(pygame.transform.scale(screen, win.get_rect().size), (0, 0))
+    drawText(win,"Welcome to Snake",28,'Roboto-Bold.ttf',WHITE,center="x")
+    drawText(win, "Press any key to start game!", 14, 'Roboto-Bold.ttf', WHITE,
+             center="x",padding=(0,80))
+    pygame.display.update()
+    start = True
+    while start == True:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:  # or event.type == pygame.KEYUP
+                if event.key == pygame.K_q:
+                    run = False
+                else:
+                    game = True
+                    score = 0
+                    start = False
 
 def main():
-    global s, win, f
-    #pygame.init()
-    win = pygame.display.set_mode((max_x*scale_factor,max_y*scale_factor)) # pixel scaling *6 on display
-    screen = pygame.Surface((max_x,max_y)) # put screen on window to allow for scaling
-    #game = GameManager()
-    #game.start()
-    s = Snake()
-    f = Fruits()
-    clock = pygame.time.Clock()
+    global s, win, f, run, score,high, level, game, start
     run = True
+    start = True
     while run:
-        pygame.time.delay(20)
-        clock.tick(30)
-        drawWindow(screen)
-        s.move()
+        win = pygame.display.set_mode((max_x*scale_factor,max_y*scale_factor)) # pixel scaling *6 on display
+        screen = pygame.Surface((max_x,max_y)) # put screen on window to allow for scaling
+        pygame.init()
+        startmenu(screen)
+        s = Snake()
+        f = Fruits()
+        f.add_random()
+        while game:
+            clock = pygame.time.Clock()
+            score += 1
+            speed, level = speedupdate()
+            pygame.time.delay(5)
+            clock.tick(speed)
+            drawWindow(screen)
+            s.move()
+        with open("HighScore.txt","r") as f:
+            high = int(f.read())
+            if score > high:
+                high = score
+                f = open("HighScore.txt","w")
+                f.write(str(high))
+                f.close()
+        drawLoseWindow(screen)
+        pygame.time.delay(4000)
+
 main()
